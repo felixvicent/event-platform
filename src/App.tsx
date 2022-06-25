7cb608b143213c5c89638ed9c589a4ca1c024af1
felixvicent <felixvicent1306@gmail.com>
2022-06-25 10:17:11 -0300

    app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
+
+function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <Router />
+      </ApolloProvider>
+    </div>
+  )
+}
+
+export default App
